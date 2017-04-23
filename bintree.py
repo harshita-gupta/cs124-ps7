5e41ab42c9a9fda18f79b3d93397abf3d27ec13d
harshita-gupta <gupta.harshita.001@gmail.com>
2017-04-23 07:56:51 -0400

    tried some binary thing lol

---
+++ bintree.py
@@ -0,0 +1,35 @@
+def isChild(x, y):
+    x == 2 * y or x == 2 * y + 1
+
+
+def isParent(x, y):
+    isChild(y, x)
+
+
+def edgeexists(x, y):
+    isChild(x, y) or isParent(x, y)
+
+
+# the above function in one statement
+def list_powerset(lst):
+    return reduce(lambda rslt, x: rslt + [subset + [x] for subset in rslt],
+                  lst, [[]])
+
+
+for trial in range(12):
+    nodes = []
+    for i in range(trial):
+        nodes.append(i)
+
+    indepsets = []
+
+    for st in list_powerset(nodes):
+        tracker = False
+        for v in st:
+            for w in st:
+                if edgeexists(v, w):
+                    tracker = True
+                    continue
+        if not tracker:
+            indepsets.append(st)
+    print "n = %d, numSets = %d" % (trial, len(indepsets))
